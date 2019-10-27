b5977e557fa068e7079898703851f6bb89fe1c8f
Maverick <bredren10@gmail.com>
2019-10-27 11:13:54 +0100

    Display posts in post views

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
+
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'post_list.html')
+    posts = Post.objects.all().order_by('-pub_date')
+    context = {'posts': posts}
+    return render(request, 'post_list.html', context)
