43c0c5b118135398116a44294f76d84ae3c62f15
Maverick <bredren10@gmail.com>
2019-10-27 11:24:16 +0100

    Truncate body text

--- posts/models.py
+++ posts/models.py
@@ -13,3 +13,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def truncate_body(self):
+        if (len(self.body) > 100):
+            return f'{self.body[:100]}...'
+        return self.body
